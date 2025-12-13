388e29d843012d08676edc851c7753dcff0b60ea
Aleksey Schastlivtsev <matuation@gmail.com>
2025-12-13 20:31:31 +0300

    changed MainApplication.java added Box.java class

---
+++ src/main/java/com/matuation/Box.java
@@ -0,0 +1,22 @@
+package com.matuation;
+
+public class Box {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Box() {
+    }
+
+    public Box(int size) {
+        this.size = size;
+    }
+
+
+}
--- src/main/java/com/matuation/MainApplication.java
+++ src/main/java/com/matuation/MainApplication.java
@@ -3,7 +3,11 @@ package com.matuation;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
-        System.out.println("111111");
+        System.out.println("1001");
 
+    }
+
+    public static void doSomething(String[] args) {
+        System.out.println("1");
     }
 }
