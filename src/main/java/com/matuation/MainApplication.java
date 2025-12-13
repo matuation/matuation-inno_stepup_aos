1a225e96a375a010715d9d737189a237b51a1191
Aleksey Schastlivtsev <matuation@gmail.com>
2025-12-13 20:20:06 +0300

    changed MainApplication.java

---
+++ src/main/java/com/matuation/MainApplication.java
@@ -0,0 +1,9 @@
+package com.matuation;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        System.out.println("111111");
+
+    }
+}
