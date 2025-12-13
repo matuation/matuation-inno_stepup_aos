2f6cfc2256b796e2e3f92ec2b4fdf8d9160a37a7
Aleksey Schastlivtsev <matuation@gmail.com>
2025-12-13 20:58:50 +0300

    improved MainApplication.java a lot

--- src/main/java/com/matuation/MainApplication.java
+++ src/main/java/com/matuation/MainApplication.java
@@ -2,14 +2,15 @@ package com.matuation;
 
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("---");
         System.out.println("Hello and welcome!");
         System.out.println("1001");
 
     }
 
     public static void doSomething(String[] args) {
+        System.out.println("0");
         System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+        System.out.println("1");
     }
 }
